ec480a87f1311619197515754dbbf7eef3962538
Enzo Khamprasong <22503774@me.uvsq.fr>
2026-04-02 11:05:40 +0200

    Ajout du deuxième test unitaire pour statut Faux

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -4,6 +4,7 @@ from cashpro.reduction import *
 class calculerReductionUnitTest(unittest.TestCase):
   def test_exemple(self):
     self.assertEqual(calculerReduction(50, False), 0)
+    self.assertEqual(calculerReduction(200, False), 0)
 
 if __name__ == '__main__':
   unittest.main()
