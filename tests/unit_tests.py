e51c9e43c89818f664c1d6a5c8d83ddbab84d133
Enzo Khamprasong <22503774@me.uvsq.fr>
2026-04-02 11:06:48 +0200

    Ajout des 4 tests unitaires avec statut Vrai

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -5,6 +5,9 @@ class calculerReductionUnitTest(unittest.TestCase):
   def test_exemple(self):
     self.assertEqual(calculerReduction(50, False), 0)
     self.assertEqual(calculerReduction(200, False), 0)
-
+    self.assertEqual(calculerReduction(15, True), 0)
+    self.assertEqual(calculerReduction(32, True), 0)
+    self.assertEqual(calculerReduction(100, True), 0)
+    self.assertEqual(calculerReduction(201, True), 0)
 if __name__ == '__main__':
   unittest.main()
